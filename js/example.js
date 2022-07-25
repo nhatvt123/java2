07026f4a14f4fd4afb39591a19b438d527c172d9
nhatvt123 <tuannhatfpt@gmail.com>
2022-07-25 20:51:13 +0700

    bt2

---
+++ js/example.js
@@ -0,0 +1,11 @@
+el.textContent = welcome;
+var elSing = document.getElementById('userSign');
+elSing.textContent= sign;
+var elTiles = document.getElementById('tiles');
+elTiles.textContent = title;
+var elSubTotal = document.getElementById('subTotal');
+elSubTotal.textContent= '$'+subTotal;
+var elShipping = document.getElementById('shipping');
+elShipping.textContent= '$'+ shipping;
+var elGrandTotal = document.getElementById('grandTotal');
+elGrandTotal.textContent= '$'+ grandTotal;
---
+++ js/update-array.js
@@ -0,0 +1,8 @@
+var colors = ['white','black','custom'];
+
+colors[2] = 'begie';
+
+var el = document.getElementById('colors');
+
+el.textContent = colors[2];
+
